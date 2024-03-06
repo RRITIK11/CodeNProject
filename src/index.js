e1cffa82b580387b1418653fbab110cf31b11cc6
RRITIK11 <sharmaritik5550@gmail.com>
2024-03-06 22:50:39 +0530

    database connected

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-// require('dotenv').config({path: './env'})
+// require('dotenv').config({path: './env'}) 
+
 import connectDB from "./db/index.js"
 import dotenv from "dotenv"
+import express from "express"
 
 dotenv.config({
     path: './env'
 })
 
+const app = express();
+
 connectDB()
 .then(()=>{
     app.listen(process.env.PORT || 8000, ()=>{
         console.log(`Server is running at PORT : ${process.env.PORT}`)
+    })
+    app.get('/',(req,res)=>{
+        res.send("Hello jee kaise h app jb")
     })
 })
 .catch((err)=>{
